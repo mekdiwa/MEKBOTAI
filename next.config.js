f75a9c019df325fb617046af168c17d737c9ba51
mekdiwa <cud753614@gmail.com>
2026-08-24 21:16:05 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,   // ข้าม Warning ตอน Build
+  },
+  typescript: {
+    ignoreBuildErrors: true,    // เผื่อใช้ TypeScript แล้วมี Error
+  },
 }
 
 module.exports = nextConfig
